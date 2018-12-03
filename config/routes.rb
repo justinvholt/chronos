852679bb2d67b4c2d471a1533fc51c5b06ec7607
Richard van den Broek <vandenbroek.rp@gmail.com>
2018-12-03 17:05:09 -0200

    optimizing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  resources :fixtures, only: [:index, :new, :create, :update, :edit] do
-    resources :calculation, only: [:show, :update, :edit, :update] do
+
+  resources :fixtures do
+    resources :demurrage, only: [:edit, :update] do
       resources :cargo_events, only: [:new, :create]
     end
   end
