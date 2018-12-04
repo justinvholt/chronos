e1ebae0e13fb9984efc414a6052baf49212bfed3
Steven Bowen <sdbowen.git@gmail.com>
2018-12-04 01:48:12 -0200

    Add fixture cargo to new fixture form creation

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -1,4 +1,8 @@
 class Fixture < ApplicationRecord
   belongs_to :demurrage_calculation
   belongs_to :fixture_clause
+  has_many :fixture_cargos, inverse_of: :fixture
+
+  # Required for cocoon nested forms gem
+  accepts_nested_attributes_for :fixture_cargos, reject_if: :all_blank, allow_destroy: true
 end
