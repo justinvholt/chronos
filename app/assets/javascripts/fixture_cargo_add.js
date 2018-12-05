4a34e3081bc903d5f0f9b7c4ce528db17de5ac71
Steven Bowen <sdbowen.git@gmail.com>
2018-12-05 12:44:48 -0200

    Place cargo entries under correct header on edit page

--- app/assets/javascripts/fixture_cargo_add.js
+++ app/assets/javascripts/fixture_cargo_add.js
@@ -1,6 +1,7 @@
 // Add event listener for DOM entries from cocoon nested form
-// and append them onto the correct header
+// and append them onto the correct header.
 
+// Create new form
 $(".new-fixture__add-cargos").on("cocoon:after-insert", function(
   e,
   insertedItem
@@ -16,3 +17,22 @@ $(".new-fixture__add-cargos").on("cocoon:after-insert", function(
   const dischargeHeader = document.querySelector(".fixture-view__discharge");
   dischargeHeader.appendChild(newCargoDischargeEntry);
 });
+
+// Edit form
+
+// Get all cargo entries
+cargoEntries = document.querySelector(".new-fixture__add-cargos").children;
+
+// Loop through cargo entires and ammend entries to their correct headers
+// based on the ID of the entry row.
+for (let element of cargoEntries) {
+  const loadingHeader = document.querySelector(".fixture-view__loading");
+  const dischargeHeader = document.querySelector(".fixture-view__discharge");
+
+  if (element.id == "loading") {
+    loadingHeader.appendChild(element);
+  }
+  if (element.id == "discharging") {
+    dischargeHeader.appendChild(element);
+  }
+}
