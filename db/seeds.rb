74ac8b584f52eaf8b0f5b128bc4ea95ef798da1f
Justin Holt <justinvholt@gmail.com>
2018-12-05 19:44:18 -0200

    Redirect to fixtures#index if user signed in (devise issue)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,6 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 
-puts "------------------------------------------------------------"
 puts "seeding user"
 user_count = 0
 
