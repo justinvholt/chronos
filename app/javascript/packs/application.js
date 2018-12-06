cabb28b2fa3d77cd1099c7330814c983d96a3683
Justin Holt <justinvholt@gmail.com>
2018-12-06 15:14:08 -0200

    Updated clauses#index formatting; tinkered with devise modals

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,26 @@
 //=require bootstrap
+import $ from 'jquery';
+
 import {
   createNewCargoItemForm,
-  editCargoItemForm
+  editCargoItemForm,
+  flip_dialog
 } from "../views/fixture_cargo_add";
 
 createNewCargoItemForm();
 editCargoItemForm();
+
+// import {
+//   flip_dialog
+// } from "../views/fixture_cargo_add";
+
+$(function() {
+  $('#flip1').click(function() {
+      flip_dialog();
+  });
+
+  $('#flip2').click(function() {
+      flip_dialog();
+  });
+
+});
