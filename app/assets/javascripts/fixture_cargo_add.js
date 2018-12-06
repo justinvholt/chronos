ee2eab18cbf066eee4497f5575db6da277af23d9
Steven Bowen <sdbowen.git@gmail.com>
2018-12-05 23:10:59 -0200

    Add comments

--- app/assets/javascripts/fixture_cargo_add.js
+++ app/assets/javascripts/fixture_cargo_add.js
@@ -1,3 +1,9 @@
+// Add event listener for DOM entries from cocoon nested form
+// and append them onto the correct header.
+// As new fixture_cargo items are added by the user
+// this script moves the items sections (Loading and Discharge)
+// under the correct DOM header.
+
 $(".new-fixture__add-cargos").on("cocoon:after-insert", function(
   e,
   insertedItem
