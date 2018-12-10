3692a1c0738b87e6c73c6e3abdf53dd05744c2f4
Richard van den Broek <vandenbroek.rp@gmail.com>
2018-12-10 16:13:37 -0200

    created rough demurrage summary in events index file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,10 +10,13 @@ class EventsController < ApplicationController
     set_clause_group
     assess_events_clauses
     assess_events_laytime
+    cumilative_laytime
+    calculate_demurrage
   end
 
   def index
     all_fixture_events
+    set_allowed_laytime
     group_events
     run_chronos
     @port_events
@@ -27,17 +30,6 @@ class EventsController < ApplicationController
         events.each do |event|
           @event = event
           assess_clauses(event)
-        end
-      end
-    end
-  end
-
-  def assess_events_laytime
-    @port_events.each do |port, terminals|
-      terminals.each do |terminal, events|
-        events.each do |event|
-          @event = event
-          calculate_laytime(event)
         end
       end
     end
@@ -59,6 +51,17 @@ class EventsController < ApplicationController
     end
   end
 
+  def assess_events_laytime
+    @port_events.each do |port, terminals|
+      terminals.each do |terminal, events|
+        events.each do |event|
+          @event = event
+          calculate_laytime(event)
+        end
+      end
+    end
+  end
+
   def calculate_laytime(event)
     @start_datetime = event.datetime if event.counting == "Laytime starts"
     @end_datetime = event.datetime if event.counting == "Laytime stops"
@@ -66,6 +69,39 @@ class EventsController < ApplicationController
       @event.laytime = TimeDifference.between(@end_datetime, @start_datetime).in_minutes
     end
   end
+
+  def cumilative_laytime
+    @total_laytime = 0.to_f
+
+    @port_events.each do |port, terminals|
+      @port = port
+      terminals.each do |terminal, events|
+        events.each do |event|
+          if event.laytime > 0
+            @laytime_summary[@port][:used] += event.laytime
+            @total_laytime += event.laytime
+          end
+        end
+      end
+    end
+    @laytime_summary[:total][:used] = @total_laytime
+    time_on_demurrage
+  end
+
+  def time_on_demurrage
+    @laytime_summary.each do |port, mins|
+      if mins[:used] != nil && mins[:allowed] != nil
+        on_demurrage = mins[:used] - mins[:allowed]
+        @laytime_summary[port][:on_dem] = on_demurrage if on_demurrage > 0.to_f
+      end
+    end
+  end
+
+  def calculate_demurrage
+    @demurrage = @laytime_summary[:total][:on_dem] * ( @fixture.demurrage_rate / 1440 )
+  end
+
+  ## setting up
 
   def all_fixture_events
     @handlings = Set.new
@@ -97,5 +133,14 @@ class EventsController < ApplicationController
 
   def set_fixture
     @fixture = Fixture.find(params[:fixture_id])
+  end
+
+  def set_allowed_laytime
+    @laytime_summary = Hash.new
+    @cargoes.each do |cargo|
+      @laytime_summary[cargo.load_port] = {used: 0.to_f}
+      @laytime_summary[cargo.disch_port] = {used: 0.to_f}
+    end
+    @laytime_summary[:total] = {allowed: @fixture.allowed_laytime.to_f * 60}
   end
 end
